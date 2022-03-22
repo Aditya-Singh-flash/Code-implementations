875553ecbfc580f5d155c2aa634214d435ee98c3
Aditya Singh <aditya2407singh@gmail.com>
2022-03-22 10:45:57 +0530

    Insertion at given position done

--- Linked_list_basics.cpp
+++ Linked_list_basics.cpp
@@ -1,20 +1,24 @@
 /* Program contains the following:
 1. Creation of node for a linked list
-2. Insertion at head in a list
-3. Traversal of a linked list
-4. Insertion at end(tail) of a linked list   */
+2. Insertion at head in a list                                      Time complexity- O(1)
+3. Traversal of a linked list                                       Time complexity- O(n)
+4. Insertion at end(tail) of a linked list                          Time complexity- O(n)
+5. Insertion at a given position in a linked list                   Time complexity- Best[O(1)]-insertion at head
+                                                                                     Worst[O(n)]-insertion at end 
+*/
 
 
 #include<iostream>
 using namespace std;
-//Creating a node class 
+//1. Creating a node class 
  class node{                  
      public:      //declared public as it needs to be accessed by all
      int data;
      node* next;
  };
  node* head=NULL;  //declaring head global so that it can be accessed from anywhere
- void Insert_head(int d){
+ //2. Insertion at head
+ void insert_head(int d){
      node* temp=new node;    //creation of a temporary node
          temp->data=d;       //storing data in temporary node
          if(head==NULL){     //check if list is blank
@@ -26,6 +30,7 @@ using namespace std;
              head=temp;
          }
      }
+//3. Traversal of linked list
 void traverse(node* h){     //Linked list traversal
     node* curr=h;            //taking a temporary node curr so that head is not lost
     while(curr!=NULL){           //as curr is a pointer it shouldn't be null
@@ -33,7 +38,8 @@ void traverse(node* h){     //Linked list traversal
         curr=curr->next;
     }
     cout<<endl;
-} 
+}
+//4. Insertion at tail/end 
 void insert_end(int d){    //Insert at end function
     node* temp=new node;    //Temporary node creation
     temp->data=d;
@@ -49,16 +55,39 @@ void insert_end(int d){    //Insert at end function
         curr->next=temp;  // curr will be at the last node after the end of while loop
     }
 }
+//5. Insertion at a given position
+void insert_Pos(int pos,int d){
+    node* temp=new node;   //creation of a new node
+    temp->data=d;
+    temp->next=NULL;
+    if(pos<=1)           // Condition if user wants to insert at head
+    insert_head(d);
+    else{
+        node* pred= head;   //A temporary pointer for traversal as we can't lose head
+        int i=1;            //i=1 because we count it from 1 and go till (pos-1)
+        while(i<(pos-1) && pred!=NULL){
+            pred=pred->next; 
+            i++;
+        }
+        if(pred==NULL)
+        cout<<"Invalid position as linked list is smaller than requested position";
+        else{
+            temp->next=pred->next;
+            pred->next=temp;
+        }
+    }
+}
 //main function
  int main(){  
-     Insert_head(5);
-     Insert_head(10);
-     Insert_head(15);
+     insert_head(5);
+     insert_head(10);
+     insert_head(15);
      cout<<head->data<<" "<<head->next->data<<" "<<head->next->next->data<<endl;
      traverse(head);
      insert_end(10);
      insert_end(15);
      insert_end(25);
+     insert_Pos(4,65);
      traverse(head);
 
      return 0;
---
+++ delete.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+//Creating a node class 
+ class node{                  
+     public:      //declared public as it needs to be accessed by all
+     int data;
+     node* next;
+ };
+ node* head=NULL;
+int main(){
+    return 0;
+}
